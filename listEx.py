2c9357aacda06ea3f385d27c2da24a25c211baa8
MylesH-P <34037785+MylesH-P@users.noreply.github.com>
2019-07-19 11:51:09 -0400

    Update listEx.py

--- listEx.py
+++ listEx.py
@@ -5,9 +5,12 @@ def main():
     for i in range(5):
         grub = input("type your food now!")
         food.append(grub)
+        print("the len of this list right now is ", len(food))
     print("nice list!")
     print(food)
     print("The third food on your list was ", food[2])
+
+    print("The len now is ", len(food))
 # DON'T TOUCH! Setup code that runs your main() function.
 if __name__ == "__main__":
  main()
