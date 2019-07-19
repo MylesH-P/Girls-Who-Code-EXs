19a1bd04e989a6320a497dfc485c7296a0019a8e
MylesH-P <34037785+MylesH-P@users.noreply.github.com>
2019-07-19 11:43:46 -0400

    Add files via upload

---
+++ listEx.py
@@ -0,0 +1,13 @@
+def main():
+    print("hi :)")
+    food = []
+    print("Hey future Myles, input 3 foods!")
+    for i in range(5):
+        grub = input("type your food now!")
+        food.append(grub)
+    print("nice list!")
+    print(food)
+    print("The third food on your list was ", food[2])
+# DON'T TOUCH! Setup code that runs your main() function.
+if __name__ == "__main__":
+ main()
